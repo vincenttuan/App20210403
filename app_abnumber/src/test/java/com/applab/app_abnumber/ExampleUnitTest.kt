faaf34ebe0571791791457c635df727cd036b9ec
MB-teacher <vincenttuan@gmail.com>
2021-04-03 15:58:58 +0800

    App

--- app_abnumber/src/test/java/com/applab/app_abnumber/ExampleUnitTest.kt
+++ app_abnumber/src/test/java/com/applab/app_abnumber/ExampleUnitTest.kt
@@ -1,8 +1,10 @@
 package com.applab.app_abnumber
 
+import com.applab.app_abnumber.utils.ABNumber
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.util.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +15,20 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun testAB() {
+        val ab = ABNumber()
+        ab.setAns()
+        println(Arrays.toString(ab.getResult("1234")))
+        println(Arrays.toString(ab.getResult("5678")))
+        println(Arrays.toString(ab.getResult("5612")))
+        println(Arrays.toString(ab.getResult("5634")))
+        println(Arrays.toString(ab.getResult("5217")))
+        println(Arrays.toString(ab.getResult("5372")))
+        println(Arrays.toString(ab.getResult("5743")))
+        println(Arrays.toString(ab.getResult("5782")))
+        println(Arrays.toString(ab.getResult("5842")))
     }
 }
