507ac2667fcf9aff13b9c6da4a54642ee99ae3ca
MB-teacher <vincenttuan@gmail.com>
2021-04-03 16:07:55 +0800

    App

--- app_abnumber/src/main/java/com/applab/app_abnumber/MainActivity.kt
+++ app_abnumber/src/main/java/com/applab/app_abnumber/MainActivity.kt
@@ -2,10 +2,24 @@ package com.applab.app_abnumber
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.applab.app_abnumber.utils.ABNumber
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private var ab = ABNumber()
+    private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ab.setAns()
     }
+
+    fun onClickSubmitButton(view: View) {
+        val guess = "1234"
+        val result = ab.getResult(guess)
+        Log.d("MainActivity", Arrays.toString(result))
+    }
+
 }
