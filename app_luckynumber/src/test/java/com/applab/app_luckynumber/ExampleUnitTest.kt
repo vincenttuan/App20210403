14afb7fa4acaeacfb2b394d623133f45b4460f24
MB-teacher <vincenttuan@gmail.com>
2021-04-03 10:35:19 +0800

    App

--- app_luckynumber/src/test/java/com/applab/app_luckynumber/ExampleUnitTest.kt
+++ app_luckynumber/src/test/java/com/applab/app_luckynumber/ExampleUnitTest.kt
@@ -1,5 +1,6 @@
 package com.applab.app_luckynumber
 
+import com.applab.app_luckynumber.utils.LuckyNumber
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -13,5 +14,26 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun test_luckyNumber() {
+        val luckyNumber = LuckyNumber()
+        val ans = luckyNumber.ans
+        val guess = 7
+        val result = luckyNumber.validate(guess)
+        println("ans: $ans, guess: $guess, result: $result")
+        for (i in 1..9) {
+            val guess = i
+            val result = luckyNumber.validate(guess)
+            println("ans: $ans, guess: $guess, result: $result")
+            if(guess > ans) {
+                assertTrue(result > 0)
+            } else if(guess < ans){
+                assertTrue(result < 0)
+            } else {
+                assertTrue(result == 0)
+            }
+        }
     }
 }
