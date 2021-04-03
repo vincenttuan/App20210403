8127efdd51be5f1692ed8049ad617a1efdde94d4
MB-teacher <vincenttuan@gmail.com>
2021-04-03 12:07:27 +0800

    App

--- app_luckynumber/src/main/java/com/applab/app_luckynumber/MainActivity.kt
+++ app_luckynumber/src/main/java/com/applab/app_luckynumber/MainActivity.kt
@@ -2,10 +2,33 @@ package com.applab.app_luckynumber
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.applab.app_luckynumber.utils.LuckyNumber
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val luckyNumber = LuckyNumber()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    // 按下 submit button 後所要執行的邏輯
+    fun onClickSubmitButton(view: View) {
+        val guess = inputNumber.text.toString().toInt()
+        //val ans = luckyNumber.ans
+        val result = luckyNumber.validate(guess)
+        if(result > 0) {
+            // 通知 Toast
+            Toast.makeText(this, R.string.too_big, Toast.LENGTH_SHORT).show()
+        } else if(result < 0) {
+            Toast.makeText(this, R.string.too_small, Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, R.string.bingo, Toast.LENGTH_SHORT).show()
+            //finish() // Activity運行結束
+        }
     }
 }
