eba2ee05304543c0035da7fdcede7954c17f7dd5
MB-teacher <vincenttuan@gmail.com>
2021-04-03 12:13:31 +0800

    App

--- app_luckynumber/src/main/java/com/applab/app_luckynumber/MainActivity.kt
+++ app_luckynumber/src/main/java/com/applab/app_luckynumber/MainActivity.kt
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.applab.app_luckynumber.utils.LuckyNumber
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    val luckyNumber = LuckyNumber()
+    private var luckyNumber = LuckyNumber()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,10 +26,29 @@ class MainActivity : AppCompatActivity() {
         if(result > 0) {
             // 通知 Toast
             Toast.makeText(this, R.string.too_big, Toast.LENGTH_SHORT).show()
+            // 通知 AlertDialog
+            AlertDialog.Builder(this)
+                .setTitle(R.string.title)
+                .setMessage(R.string.too_big)
+                .setPositiveButton(R.string.ok, null)
+                .show()
+
         } else if(result < 0) {
             Toast.makeText(this, R.string.too_small, Toast.LENGTH_SHORT).show()
+            AlertDialog.Builder(this)
+                .setTitle(R.string.title)
+                .setMessage(R.string.too_small)
+                .setPositiveButton(R.string.ok, null)
+                .show()
         } else {
             Toast.makeText(this, R.string.bingo, Toast.LENGTH_SHORT).show()
+            AlertDialog.Builder(this)
+                .setTitle(R.string.title)
+                .setMessage(R.string.bingo)
+                .setPositiveButton(R.string.ok, null)
+                .show()
+            // 重新產生 LuckyNumber 物件
+            luckyNumber = LuckyNumber()
             //finish() // Activity運行結束
         }
     }
