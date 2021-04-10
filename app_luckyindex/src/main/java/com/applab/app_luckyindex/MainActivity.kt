5254dd8f428f4cd8b5d7b56c88beeb6e600208c8
MB-teacher <vincenttuan@gmail.com>
2021-04-10 13:43:30 +0800

    App

--- app_luckyindex/src/main/java/com/applab/app_luckyindex/MainActivity.kt
+++ app_luckyindex/src/main/java/com/applab/app_luckyindex/MainActivity.kt
@@ -1,18 +1,13 @@
 package com.applab.app_luckyindex
 
-import android.app.Activity
-import android.app.Instrumentation
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : ComponentActivity() {
@@ -29,6 +24,7 @@ class MainActivity : ComponentActivity() {
         val intent = Intent(context, ResultActivity::class.java)
         // 將 input_number 所輸入的資料傳送到下一個 Activity (也就是 ResultActivity)
         intent.putExtra("number", input_number.text.toString())
+        // 轉跳 Activity
         openResultActivityCustom.launch(intent)
 
     }
@@ -36,7 +32,9 @@ class MainActivity : ComponentActivity() {
     private val openResultActivityCustom =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == 101) {
-                Toast.makeText(this, result.data?.getStringExtra("luckyIndex").toString(), Toast.LENGTH_SHORT).show()
+                Toast.makeText(context,
+                               result.data?.getStringExtra("luckyIndex").toString(),
+                               Toast.LENGTH_SHORT).show()
             }
         }
 
