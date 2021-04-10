2557e64e83967064ec84b0cf6f2a79b198a13cca
MB-teacher <vincenttuan@gmail.com>
2021-04-10 15:17:05 +0800

    App

--- app_exchange/src/main/java/com/applab/app_exchange/ExchangeActivity.kt
+++ app_exchange/src/main/java/com/applab/app_exchange/ExchangeActivity.kt
@@ -2,10 +2,30 @@ package com.applab.app_exchange
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_exchange.*
+import yahoofinance.YahooFinance
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ExchangeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exchange)
+
+        Thread(Runnable {
+            val amount = intent.getIntExtra("amount", 0)
+            val usd = getUSDByTWD(amount)
+            runOnUiThread(Runnable {
+                result_amount.text = "%.2f".format(usd)
+                val time = SimpleDateFormat("yyyy/MM/dd HH:mm:ss", Locale.TAIWAN)
+                result_time.text = time.format(Date())
+            })
+        }).start()
+
+    }
+
+    fun onClick(view: View) {
+        finish()
     }
 }
--- app_exchange/src/main/java/com/applab/app_exchange/MainActivity.kt
+++ app_exchange/src/main/java/com/applab/app_exchange/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.applab.app_exchange
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var context: Context
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        context = this
+    }
+
+    fun onClick(view: View) {
+        val intent = Intent(context, ExchangeActivity::class.java)
+        // 將 input_number 所輸入的資料傳送到下一個 Activity (也就是 ResultActivity)
+        intent.putExtra("amount", input_amount.text.toString().toInt())
+        startActivity(intent)
     }
 }
