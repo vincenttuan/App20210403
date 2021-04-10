3c9fe8ec98b61b796cbafb0532d04c9ebba9d117
MB-teacher <vincenttuan@gmail.com>
2021-04-10 11:47:15 +0800

    App

--- app_luckyindex/src/main/java/com/applab/app_luckyindex/MainActivity.kt
+++ app_luckyindex/src/main/java/com/applab/app_luckyindex/MainActivity.kt
@@ -4,11 +4,13 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var context: Context
-
+    val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,7 +18,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onClick(view: View) {
+        Log.d(TAG, input_number.text.toString())
         val intent = Intent(context, ResultActivity::class.java)
+        // 將 input_number 所輸入的資料傳送到下一個 Activity (也就是 ResultActivity)
+        intent.putExtra("number", input_number.text.toString())
         startActivity(intent)
     }
 }
--- app_luckyindex/src/main/java/com/applab/app_luckyindex/ResultActivity.kt
+++ app_luckyindex/src/main/java/com/applab/app_luckyindex/ResultActivity.kt
@@ -2,10 +2,20 @@ package com.applab.app_luckyindex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_result.*
+import java.util.*
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+        // 接收 MainActivity 所傳來的 number 資料
+        val number = intent.getStringExtra("number").toString().toInt()
+        // 取得 Lucky Index 的資料
+        val luckyIndex = getLuckyIndex(number)
+        // 將 luckyIndex 放到 View元件(text_result)中
+        text_result.text = luckyIndex.toString()
+        // 將現在時間放到 View元件(text_time)中
+        text_time.text = Date().toString()
     }
 }
