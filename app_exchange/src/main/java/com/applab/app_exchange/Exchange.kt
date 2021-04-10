dd4c99dc2d74730131ae43241356fc8905e30fe3
MB-teacher <vincenttuan@gmail.com>
2021-04-10 16:31:05 +0800

    App

--- app_exchange/src/main/java/com/applab/app_exchange/Exchange.kt
+++ app_exchange/src/main/java/com/applab/app_exchange/Exchange.kt
@@ -5,3 +5,8 @@ fun getUSDByTWD(amount: Int): Double {
     val rate = YahooFinance.get("TWDUSD=x").quote.price.toDouble()
     return amount * rate
 }
+
+fun getAmountByTWDAndSymbol(amount: Int, symbol: String): Double {
+    val rate = YahooFinance.get("TWD${symbol}=x").quote.price.toDouble()
+    return amount * rate
+}
--- app_exchange/src/main/java/com/applab/app_exchange/ExchangeActivity.kt
+++ app_exchange/src/main/java/com/applab/app_exchange/ExchangeActivity.kt
@@ -14,12 +14,15 @@ class ExchangeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_exchange)
         // 接收 amount 資料
         val amount = intent.getIntExtra("amount", 0)
+        // 接收 symbol 資料
+        val symbol = intent.getStringExtra("symbol").toString()
 
         // 建立子執行緒
         Thread(Runnable {
             // 子執行緒環境中才能執行網路程式
             // YahooFinance API 是一種網路程式
-            val usd = getUSDByTWD(amount)
+            //val usd = getUSDByTWD(amount)
+            val usd = getAmountByTWDAndSymbol(amount, symbol)
 
             // 變更 UI View 元件內容, 必須要在 UI 執行緒環境下
             runOnUiThread(Runnable {
@@ -27,6 +30,7 @@ class ExchangeActivity : AppCompatActivity() {
                 result_amount.text = "%.2f".format(usd)
                 val time = SimpleDateFormat("yyyy/MM/dd HH:mm:ss", Locale.TAIWAN)
                 result_time.text = time.format(Date())
+                text_symbol.text = "${text_symbol.text} ${symbol}"
             })
 
         }).start()
