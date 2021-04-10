2c347b88c7408371ce0d607f3aaef5ac4f0d408f
MB-teacher <vincenttuan@gmail.com>
2021-04-10 15:20:47 +0800

    App

--- app_exchange/src/main/java/com/applab/app_exchange/ExchangeActivity.kt
+++ app_exchange/src/main/java/com/applab/app_exchange/ExchangeActivity.kt
@@ -12,15 +12,22 @@ class ExchangeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exchange)
+        // 接收 amount 資料
+        val amount = intent.getIntExtra("amount", 0)
 
+        // 建立子執行緒
         Thread(Runnable {
-            val amount = intent.getIntExtra("amount", 0)
+            // 子執行緒環境中才能執行網路程式
+            // YahooFinance API 是一種網路程式
             val usd = getUSDByTWD(amount)
+            // UI 執行緒環境
+            // 變更 UI View 元件內容, 必須要在 UI 執行緒環境下
             runOnUiThread(Runnable {
                 result_amount.text = "%.2f".format(usd)
                 val time = SimpleDateFormat("yyyy/MM/dd HH:mm:ss", Locale.TAIWAN)
                 result_time.text = time.format(Date())
             })
+
         }).start()
 
     }
