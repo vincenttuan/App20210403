82df360e74e233b4148f53c22ab34cb5b9f872c5
MB-teacher <vincenttuan@gmail.com>
2021-04-17 15:24:50 +0800

    App

--- app_exam/src/main/java/com/applab/app_exam/AddActivity.kt
+++ app_exam/src/main/java/com/applab/app_exam/AddActivity.kt
@@ -2,10 +2,23 @@ package com.applab.app_exam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_add.*
 
 class AddActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
+
+    }
+
+    fun onClick(view: View) {
+        val name = et_name.text.toString()
+        val score = et_score.text.toString()
+        // 傳送資料到 MainActivity
+        intent.putExtra("name", name)
+        intent.putExtra("score", score)
+        setResult(101, intent)
+        finish() // 將 ResultActivity 畫面關閉
     }
 }
