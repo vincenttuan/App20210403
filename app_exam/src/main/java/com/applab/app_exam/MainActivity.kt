b8d36120c039d3eb02162f396e4fc432ca22b096
MB-teacher <vincenttuan@gmail.com>
2021-04-17 12:14:20 +0800

    App

--- app_exam/src/main/java/com/applab/app_exam/MainActivity.kt
+++ app_exam/src/main/java/com/applab/app_exam/MainActivity.kt
@@ -1,26 +1,46 @@
 package com.applab.app_exam
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import androidx.activity.ComponentActivity
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.Toolbar;
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var context: Context
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+        context = this
 
         // FAB
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
             //        .setAction("Action", null).show()
-            
+            val intent = Intent(context, AddActivity::class.java)
+            // 轉跳 Activity
+            startActivity(intent)
+
         }
     }
+
+    private val openResultActivityCustom =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == 101) {
+
+            }
+        }
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
