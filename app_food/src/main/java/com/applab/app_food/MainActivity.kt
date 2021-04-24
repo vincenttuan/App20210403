609a6a14ddc1d792b0082e34643f127d473aff71
MB-teacher <vincenttuan@gmail.com>
2021-04-24 14:50:41 +0800

    App

--- app_food/src/main/java/com/applab/app_food/MainActivity.kt
+++ app_food/src/main/java/com/applab/app_food/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.title.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var context: Context
