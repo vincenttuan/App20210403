1174c089fa69980de245b6b5bfd09da0f57affdd
MB-teacher <vincenttuan@gmail.com>
2021-04-24 12:32:07 +0800

    App

--- app_food/src/main/java/com/applab/app_food/MainActivity.kt
+++ app_food/src/main/java/com/applab/app_food/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.applab.app_food
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var context: Context
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        context = this
+
+        val food = Food("m41370", "地中海辣鷄黑堡", 100, true, true)
+        Toast.makeText(context, food.toString(), Toast.LENGTH_SHORT).show()
+
 
 
     }
