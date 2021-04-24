3bff81b1caf6f53dd3cb66a24680a6b1b18bee3c
MB-teacher <vincenttuan@gmail.com>
2021-04-24 16:14:28 +0800

    App

--- app_food/src/main/java/com/applab/app_food/MainActivity.kt
+++ app_food/src/main/java/com/applab/app_food/MainActivity.kt
@@ -5,6 +5,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
@@ -87,6 +89,23 @@ class MainActivity : AppCompatActivity() {
                     image_new.setImageResource(android.R.color.transparent)
                 }
             }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_food -> true
+            R.id.action_drink -> true
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
 }
