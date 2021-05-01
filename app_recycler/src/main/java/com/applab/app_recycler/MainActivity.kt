285808cf6e8f2ceac00398c8511f6f7a1b7eae7d
MB-teacher <vincenttuan@gmail.com>
2021-05-01 10:38:15 +0800

    App

--- app_recycler/src/main/java/com/applab/app_recycler/MainActivity.kt
+++ app_recycler/src/main/java/com/applab/app_recycler/MainActivity.kt
@@ -2,10 +2,14 @@ package com.applab.app_recycler
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var json = assets.open("csvjson.json").bufferedReader().use { it.readText() }
+        Log.d("MainActivity", json)
     }
 }
