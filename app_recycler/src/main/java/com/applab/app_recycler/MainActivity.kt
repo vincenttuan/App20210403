928c0180e29853c1953c6b2a56e459a43c747967
MB-teacher <vincenttuan@gmail.com>
2021-05-01 10:45:39 +0800

    App

--- app_recycler/src/main/java/com/applab/app_recycler/MainActivity.kt
+++ app_recycler/src/main/java/com/applab/app_recycler/MainActivity.kt
@@ -3,6 +3,9 @@ package com.applab.app_recycler
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.lang.reflect.Type
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +13,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var json = assets.open("csvjson.json").bufferedReader().use { it.readText() }
-        Log.d("MainActivity", json)
+        // 將 json 字串轉 List<Map<String, Object>>
+        val listType: Type = object : TypeToken<List<Map<String?, Object?>?>?>() {}.type
+        val list: List<Map<String, Object>> = Gson().fromJson(json, listType)
+        Log.d("MainActivity", list.toString())
     }
 }
