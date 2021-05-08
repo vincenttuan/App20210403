99fbc5fd5e29dd2ba76d86e9b18f61c2d14364d9
MB-teacher <vincenttuan@gmail.com>
2021-05-08 12:33:04 +0800

    App

--- app_nav1/src/main/java/com/applab/app_nav1/MainActivity.kt
+++ app_nav1/src/main/java/com/applab/app_nav1/MainActivity.kt
@@ -2,10 +2,18 @@ package com.applab.app_nav1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 手動加入 action bar
+        setSupportActionBar(toolbar)
     }
 }
