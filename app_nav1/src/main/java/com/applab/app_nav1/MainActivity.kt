ce22a74208e8ba886b97ff80df2f5004595f53ac
MB-teacher <vincenttuan@gmail.com>
2021-05-08 12:41:08 +0800

    App

--- app_nav1/src/main/java/com/applab/app_nav1/MainActivity.kt
+++ app_nav1/src/main/java/com/applab/app_nav1/MainActivity.kt
@@ -6,14 +6,33 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        // Bottom bar 切換時會變顏色
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.homeFragment)
+        )
+
         // 手動加入 action bar
         setSupportActionBar(toolbar)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    // 實現 <- 的作用
+    // 它會自動到堆疊中找出上一頁
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
