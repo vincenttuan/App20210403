c800cd7310a9d5bd5542e570be5f422072ad1d2e
MB-teacher <vincenttuan@gmail.com>
2021-05-08 11:10:49 +0800

    App

--- app_nav1/src/main/java/com/applab/app_nav1/HomeFragment.kt
+++ app_nav1/src/main/java/com/applab/app_nav1/HomeFragment.kt
@@ -1,7 +1,20 @@
 package com.applab.app_nav1
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button_login.setOnClickListener {
+            // 注意 ! 若要出現 HomeFragmentDirections 類別,
+            // 在 nav_graph.xml 配置完畢之後
+            // 按下 Build -> Rebuild Project 才會出現
+            val action = HomeFragmentDirections.actionHomeFragmentToLoginFragment()
+            findNavController().navigate(action)
+        }
+    }
 }
