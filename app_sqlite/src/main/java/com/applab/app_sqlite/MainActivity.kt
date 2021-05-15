31d6a99778c3db777228d9c6e956a79443ad9e8e
MB-teacher <vincenttuan@gmail.com>
2021-05-15 10:41:45 +0800

    App

--- app_sqlite/src/main/java/com/applab/app_sqlite/MainActivity.kt
+++ app_sqlite/src/main/java/com/applab/app_sqlite/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.applab.app_sqlite
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.applab.app_sqlite.db.DBHelper
 
 class MainActivity : AppCompatActivity() {
+    val dbHelper = DBHelper(this)
+    lateinit var context: Context
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        context = this
+
+        title = dbHelper.databaseName
     }
 }
