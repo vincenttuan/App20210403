0f534a6284f3bd7b21da06d46f7ed23ff5d8bd85
MB-teacher <vincenttuan@gmail.com>
2021-05-15 14:55:47 +0800

    App

--- app_sqlite_lucky/src/main/java/com/applab/app_sqlite_lucky/MainActivity.kt
+++ app_sqlite_lucky/src/main/java/com/applab/app_sqlite_lucky/MainActivity.kt
@@ -2,10 +2,14 @@ package com.applab.app_sqlite_lucky
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.applab.app_sqlite_lucky.db.DBHelper
 
 class MainActivity : AppCompatActivity() {
+    val dbHelper = DBHelper(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        dbHelper.writableDatabase
     }
 }
