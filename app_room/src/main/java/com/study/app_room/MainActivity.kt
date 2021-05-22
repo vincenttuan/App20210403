f7c28471b168b2f29a57e013d5b52ed7bc9b1ede
vincenttuan <vincentjava@yahoo.com.tw>
2021-05-22 10:53:52 +0800

    app

--- app_room/src/main/java/com/study/app_room/MainActivity.kt
+++ app_room/src/main/java/com/study/app_room/MainActivity.kt
@@ -3,11 +3,15 @@ package com.study.app_room
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.room.Room
+import com.github.javafaker.Faker
 import com.study.app_room.db.User
 import com.study.app_room.db.UserDatabase
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     lateinit var db: UserDatabase
@@ -29,6 +33,27 @@ class MainActivity : AppCompatActivity() {
             Log.d("MainActivity", users.toString())
             if(users.size == 0) {
                 db.userDao().insert(user1, user2)
+            }
+        }
+
+        btn_insert.setOnClickListener {
+            val faker = Faker()
+            val name = faker.name().firstName()
+            val age = Random.nextInt(30)
+            val working = Random.nextInt(2) == 0
+            val user = User(name, age, working)
+            GlobalScope.launch {
+                db.userDao().insert(user)
+            }
+            Toast.makeText(applicationContext, "Insert", Toast.LENGTH_SHORT).show()
+        }
+
+        btn_query.setOnClickListener {
+            GlobalScope.launch {
+                val users = db.userDao().getAllUsers()
+                runOnUiThread {
+                    view_result.setText(users.toString())
+                }
             }
         }
 
