458c48121152392de024e4a7faaca353afffc032
vincenttuan <vincentjava@yahoo.com.tw>
2021-05-22 15:32:33 +0800

    app

--- app_room2/src/main/java/com/study/app_room2/MainActivity.kt
+++ app_room2/src/main/java/com/study/app_room2/MainActivity.kt
@@ -22,17 +22,19 @@ class MainActivity : AppCompatActivity(), RecyclerViewAdapter.RowOnClickListener
         setContentView(R.layout.activity_main)
         context = this
 
-        db = Room.databaseBuilder(context, UserDatabase::class.java, "mydb").build()
+        db = Room.databaseBuilder(context, UserDatabase::class.java, "mydb")
+            .createFromAsset("databases/user.db")
+            .build()
 
         GlobalScope.launch {
             var users = db.userDao().getAllUsers()
-            if (users.size == 0) {
-                db.userDao().insert(
-                    User("John", 18, true),
-                    User("Mary", 19, false)
-                )
-                users = db.userDao().getAllUsers()
-            }
+//            if (users.size == 0) {
+//                db.userDao().insert(
+//                    User("John", 18, true),
+//                    User("Mary", 19, false)
+//                )
+//                users = db.userDao().getAllUsers()
+//            }
             // 配置 recyclerView
             recyclerView.apply {
                 layoutManager = LinearLayoutManager(context)
