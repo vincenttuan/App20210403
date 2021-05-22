e1b820e3206865c40a2e99cc023eaf66c89ce493
vincenttuan <vincentjava@yahoo.com.tw>
2021-05-22 10:11:07 +0800

    app

--- app_room/src/main/java/com/study/app_room/MainActivity.kt
+++ app_room/src/main/java/com/study/app_room/MainActivity.kt
@@ -2,10 +2,27 @@ package com.study.app_room
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.room.Room
+import com.study.app_room.db.User
+import com.study.app_room.db.UserDatabase
 
 class MainActivity : AppCompatActivity() {
+    lateinit var db: UserDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        db = Room.databaseBuilder(
+            applicationContext,
+            UserDatabase::class.java,
+            "mydb"
+        ).build()
+
+        val user1 = User("John", 18, true)
+        val user2 = User("Mary", 19, false)
+
+        db.userDao().insert(user1, user2)
+
+
     }
 }
